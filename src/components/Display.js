b725e84190ca51d1bd31cfe2db0a6f8a40e50727
StevanFreeborn <sfreeborn7@gmail.com>
2022-03-31 16:36:32 -0500

    removed unused constructor; commentted out unused import;

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class Display extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div id="display">
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import * as ReactDOMClient from 'react-dom/client';
+// import * as ReactDOMClient from 'react-dom/client';
 
 // const root = ReactDOMClient.createRoot(
 //   document.getElementById("root")
