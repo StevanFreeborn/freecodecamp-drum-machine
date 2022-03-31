46d1d06f581e4456bf8030c915b7aca7aa581679
StevanFreeborn <sfreeborn7@gmail.com>
2022-03-31 16:17:16 -0500

    complete;

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import * as ReactDOMClient from 'react-dom/client';
 
-const root = ReactDOMClient.createRoot(
-  document.getElementById("root")
-);
+// const root = ReactDOMClient.createRoot(
+//   document.getElementById("root")
+// );
 
-root.render(
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// using the legacy api allows the freeCodeCamp test suite to pass.
+// attempts to use the above new root api for React 18 causes test
+// suite to fail.
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
